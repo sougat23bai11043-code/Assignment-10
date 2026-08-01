8b65a51c7ed05e3fb7f3b3396500abcff06eefe8
sougat23bai11043-code <sougat.23bai11043@vitbhopal.ac.in>
2026-08-01 16:13:23 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,47 @@
+import pandas as pd
+import joblib
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load Dataset
+df = pd.read_csv("heart.csv")
+
+print("First 5 Records")
+print(df.head())
+
+print("\nDataset Shape:", df.shape)
+
+print("\nMissing Values")
+print(df.isnull().sum())
+
+# Features and Target
+X = df.drop("target", axis=1)
+y = df["target"]
+
+# Split Dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train Model
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("\nModel Accuracy:", accuracy)
+
+# Save Model
+joblib.dump(model, "model.pkl")
+
+print("\nModel saved successfully as model.pkl")
